db48cdad05c94493404e5999155b533a7bea766f
EmmanuelCasangel <30930846+EmmanuelCasangel@users.noreply.github.com>
2018-06-12 13:16:44 -0300

    a

--- Compactador/src/classes/Descompactador.java
+++ Compactador/src/classes/Descompactador.java
@@ -15,15 +15,36 @@ public class Descompactador
             
             RandomAccessFile arq = new RandomAccessFile(nArq,"rw");
             
+            
+            int qtdLixo = arq.read();
+            int qtdCods = arq.read();
+            
             int tam =(int)(long) arq.length();
             byte[] arqEmBytes= new byte[tam];
             
-            arqEmBytes = arq.readByte();
+            
+            
+            String str  = Integer.toBinaryString(byte);
+            
+            if(str.length()<8)
+                str = completaString(str);
+            
+            if(str.length()>8)
+                str = str.substring(str.length()-8, str.length());
         }
         catch (Exception e) 
 	{
             e.printStackTrace();
 	}
+    }
+    
+    private String completaString(String str)
+    {
+        while(str.length()%8!=0)
+        {
+            str = "0"+str;
+        }
+        return str;
     }
     
     
