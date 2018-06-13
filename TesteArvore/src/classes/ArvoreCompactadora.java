aeaeb8d20818194ea135669ba712318bf93d899c
Emmanuel <30930846+EmmanuelCasangel@users.noreply.github.com>
2018-06-13 18:03:44 -0300

    a

---
+++ TesteArvore/src/classes/ArvoreCompactadora.java
@@ -0,0 +1,94 @@
+package classes;
+import java.lang.reflect.*;
+import classes.arvore.*;
+
+public class ArvoreCompactadora<X> extends Arvore<X>
+{
+   
+
+    protected String[] cods; 
+    
+    public void insere(X x)
+    {
+        if(this.raiz==null)
+            this.raiz = new No(x);
+    }    
+
+    public ArvoreCompactadora (X x)
+    {
+        this.raiz = new No(x);
+    }
+    
+    public X getRaiz()
+    {
+        if (this.raiz instanceof Cloneable)
+            return super.meuCloneDeX (raiz.getInfo());
+        else
+            return this.raiz.getInfo();
+    }
+
+    public void junteSe(ArvoreCompactadora<X> outra) throws Exception
+    {
+        if (outra==null)
+            throw new Exception ("Arvore ausente");
+
+        int freq = ((Informacao)(this.raiz.getInfo())).getFreq()+((Informacao)(outra.raiz.getInfo())).getFreq();
+
+        Informacao info = new Informacao(freq);
+        
+        No novaRaiz = new No(this.raiz, (X)info, outra.raiz);
+        
+        this.raiz = novaRaiz;
+    }
+
+    public String[] novosCodigos()
+    {
+        //por enquanto deixarei codigo como string
+
+        this.cods  = new String[256];
+        
+        String cod = "";
+        novosCodigos(this.raiz, cod);
+        
+        return cods; 
+    }
+ 
+    private void novosCodigos(No atual, String c)
+    {
+        if(atual!=null)
+            {
+
+            if(((Informacao)this.raiz.getInfo()).getCod() != null)
+            {
+                cods[((Informacao)atual.getInfo()).getCod()]=c;
+            }
+            else
+            {
+                c += "0";
+                 novosCodigos(atual.getEsq(), c);
+                c = c.substring(0, c.length()-1);
+                c += "1";
+                novosCodigos(atual.getDir(), c);
+                c = c.substring(0, c.length()-1);
+            }
+        }
+
+    }
+    
+    public Object clone ()
+    {
+       ArvoreCompactadora<X> ret=null;
+
+        try
+        {
+            ret = new ArvoreCompactadora(this);
+        }
+        catch (Exception erro)
+        {} // ignoro porque o construtor de copia so lanca excecao quando o modelo for null e modelo é o this que nunca é null
+
+        return ret;
+    }
+    
+}
+
+
---
+++ TesteArvore/src/testearvore/TesteArvore.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testearvore;
+import classes.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author u17172
+ */
+public class TesteArvore {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Informacao info = new Informacao(122,3);
+        Informacao info2 = new Informacao(231,1);
+        Informacao info3 = new Informacao(545,5);
+        
+        ArvoreCompactadora arv = new ArvoreCompactadora(info); 
+        ArvoreCompactadora arv2 = new ArvoreCompactadora(info2);
+        ArvoreCompactadora arv3 = new ArvoreCompactadora(info3);
+        
+        p(arv.toString());
+        p(arv2.toString());
+        p(arv3.toString());
+        
+        try {
+            arv.junteSe(arv2);
+            arv.junteSe(arv3);
+        } catch (Exception ex) {
+            Logger.getLogger(TesteArvore.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        p(arv.toString());
+        
+        String[] cods = arv.novosCodigos();
+        
+        for(String cod : cods)
+        {
+            if(cod!=null)
+                p(cod);
+        }
+        
+    }
+    
+    public static void p(String str)
+    {
+        System.out.println(str);
+    }
+}
