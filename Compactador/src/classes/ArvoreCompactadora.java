cfc92131187173e68d1e3854cbc612b168da952c
Emmanuel <30930846+EmmanuelCasangel@users.noreply.github.com>
2018-06-15 16:20:36 -0300

    a

--- Compactador/src/classes/ArvoreCompactadora.java
+++ Compactador/src/classes/ArvoreCompactadora.java
@@ -98,14 +98,13 @@ public class ArvoreCompactadora extends Arvore<Informacao>
                  throw new Exception ("Codigo passado não esta na arvore");
         }
         //else
-        
         if(str.charAt(0)=='0')
         {
-            str = str.substring(0, str.length()-1);
+            str = str.substring(1, str.length());
             return auxGetCod(atual.getEsq(), str);              
         }
         //str.charAt(0)=='1'
-        str = str.substring(0, str.length()-1);
+        str = str.substring(1, str.length());
         return auxGetCod(atual.getDir(), str);
         
                 
